fca758770249fea0e8dfb5b2bffc2df3edcc7518
@韩宾 <806597784@qq.com>
2020-05-28 20:42:54 +0800

    添加git

---
+++ git-test-project/src/com/hb/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.hb.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
